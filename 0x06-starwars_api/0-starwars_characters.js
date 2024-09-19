7e3dab26a43448ae785b5d0d7c8744e58456d1e8
Ahadi <ahadic044@gmail.com>
2024-09-19 14:32:03 +0200

    another starwars api:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,25 @@
-#!/usr/bin/env node
-
+#!/usr/bin/node
+/**
+ * Starwars Characters
+ */
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+const movieId = process.argv[2];
+const movieURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(movieURL, (err, res, body) => {
+  if (err) console.log(err);
+  const index = 0;
+  const characters = JSON.parse(body).characters;
+  printMovieCharacter(characters, index);
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
+
+const printMovieCharacter = function (url, i) {
+  request(url[i], (err, res, body) => {
+    if (err) console.log(err);
     console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+    if (++i < url.length) {
+      printMovieCharacter(url, i++);
+    }
   });
 };
